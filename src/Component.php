164e207313c59522b921325153bb78d207498335
Vesko Kenashkov <vesko@azonmedia.com>
2020-02-13 10:49:24 +0200

    added Component file

---
+++ src/Component.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\PsrToSwoole;
+
+use Azonmedia\Components\BaseComponent;
+
+class Component extends BaseComponent
+{
+
+    protected const COMPONENT_NAME = "PSR-to-Swoole package";
+    protected const COMPONENT_URL = 'https://psr-to-swoole.packages.guzaba.org/';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
+    protected const COMPONENT_VERSION = '0.0.1';
+    protected const VENDOR_NAME = 'Azonmedia';
+    protected const VENDOR_URL = 'https://azonmedia.com';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/psr-to-swoole/docs/ErrorReference/';
+}
